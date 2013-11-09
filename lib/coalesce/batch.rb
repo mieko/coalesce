4e5fdf058d49afaf15122e5213d28fae91509f5c
Mike Owens <mike@filespanker.com>
2013-11-09 11:39:05 -0500

    Overhaul Batch, remove Batch2

--- lib/coalesce/batch.rb
+++ lib/coalesce/batch.rb
@@ -4,6 +4,7 @@ module Coalesce
 
   class Batch
     attr_reader :objects
+    attr_reader :combiners
     attr_reader :locks
     attr_reader :combined_attributes
 
@@ -13,116 +14,58 @@ module Coalesce
     include Enumerable
 
     def initialize(prototype)
-      @objects = [prototype]
-      @locks = []
-      @combined_attributes = {}
+      @objects   = [prototype]
+      @combiners = []
+      @locks     = []
     end
 
     def prototype
       @objects.first
     end
 
+    def add_object(object)
+      @objects.push(object)
+    end
+
+    def add_combiner(combiner)
+      @combiners.push(combiner)
+    end
+
+    def to_standin
+      combined = {}
+      @combiners.each do |combiner|
+        aggregate = @objects.map do |object|
+          if object.respond_to?(combiner.attribute)
+            object.send(combiner.attribute)
+          else
+            nil
+          end
+        end
+        combined[combiner.attribute] = combiner.call(aggregate)
+      end
+
+      object = prototype.dup
+
+      object.define_singleton_method(:batch) do
+        self
+      end
+
+      object.define_singleton_method(:combined_attributes) do
+        return combined
+      end
+
+      object.define_singleton_method(:combined?) do |k=nil|
+        k ? combined.has_key?(k) : !combined.empty?
+      end
+
+      combined.each do |k, v|
+        object.define_singleton_method(k) do
+          v
+        end
+      end
+
+      object
+    end
   end
 
-  class Batch2
-    attr_reader   :rules, :prototype, :keys, :objects
-    attr_accessor :max_time_delta
-    attr_reader   :locked_rule
-
-    def initialize(prototype, rules)
-      @prototype = prototype
-      @rules = rules
-      @locked_rule = nil
-      @keys = {}
-      @objects = [@prototype]
-      @suspended = false
-      initialize_combinable!
-    end
-
-    include Enumerable
-    def each(&proc)
-      return onjects.each(&proc)
-    end
-
-    def should_combine?(r)
-      return nil unless close_enough?(r)
-      if @locked_rule
-        return nil unless @locked_rule.should_combine?(self, r)
-        @locked_rule
-      else
-        @rules.find {|rule| rule.should_combine?(self, r) }
-      end
-    end
-
-    def lock!
-      @locked_rule = -> (*args) { false }
-    end
-
-    def combine!(r)
-      if rule = should_combine?(r)
-        update_combine_hash!(rule, r)
-        @locked_rule = rule if rule.lock
-        @suspended = rule.suspended
-        @objects.push(r)
-        true
-      else
-        false
-      end
-    end
-
-    def results
-      return @objects if @suspended
-
-      obj = @prototype.clone
-      append_combine_hash!(@keys, obj)
-      return [obj]
-    end
-
-    def all_share_the_same?(attr)
-      @objects.map(&(attr.to_proc)).sort.uniq == [attr]
-    end
-
-    private
-    def initialize_combinable!
-      @rules.each do |rule|
-        rule.combine.each do |comb|
-          keys[comb] = []
-          result = @prototype.respond_to?(comb) ? @prototype.send(comb) : nil
-          keys[comb].push(result) unless result.nil?
-        end
-      end
-    end
-
-    def update_combine_hash!(rule, r)
-      rule.combine.each do |comb|
-        if r.respond_to?(comb)
-          result = r.send(comb)
-          @keys[comb].push(result) unless \
-            rule.uniq.include?(comb) && @keys[comb].include?(result)
-          @keys[comb] = [@keys[comb].inject(&:merge)] if \
-            rule.merge.include?(comb)
-        end
-      end
-    end
-
-    def append_combine_hash!(hash, obj)
-      replace_attr(obj, :combined?, true)
-      replace_attr(obj, :batch, self)
-
-      hash.each do |k, v|
-        if k == :key
-          replace_attr(obj, k, combined_key_name(v))
-        else
-          replace_attr(obj, k, v.size == 1 ? v.first : v)
-        end
-      end
-    end
-
-    def replace_attr(obj, attr, val)
-      obj.define_singleton_method(attr) do
-        val
-      end
-    end
-
-  end
 end
